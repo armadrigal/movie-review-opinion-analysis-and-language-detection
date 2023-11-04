154d2edc2dbf07596101556855ec570e11aad9d3
armad-hermann <amadrigal.fismat@gmail.com>
2023-11-04 15:09:27 -0600

    se agregó opinion_es_lstm

--- API/main.py
+++ API/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 import tensorflow as tf
 import pickle
+from api.language_detector import router as language_detector
 from api.language_detector_mlp import router as language_detector_mlp
 from api.language_detector_lstm import router as language_detector_lstm
 from api.language_detector_gru import router as language_detector_gru
-from api.language_detector import router as language_detector
+from api.opinion_es_lstm import router as opinion_es_lstm
 from api.opinion_en_lstm import router as opinion_en_lstm
 from api.opinion_en_gru import router as opinion_en_gru
 from api.opinion_en_cnn import router as opinion_en_cnn
@@ -19,6 +20,7 @@ model_lstm = tf.keras.models.load_model("./Models/model_lstm.h5")
 model_gru = tf.keras.models.load_model("./Models/model_gru.h5")
 
 #Opinion Analysis Models
+model_lstm_es = tf.keras.models.load_model("./Models/model_lstm_es.h5")
 model_lstm_en = tf.keras.models.load_model("./Models/model_lstm_en.h5")
 model_gru_en = tf.keras.models.load_model("./Models/model_gru_en.h5")
 model_cnn_en = tf.keras.models.load_model("./Models/model_cnn_en.h5")
@@ -28,19 +30,24 @@ model_lstm_attention_en = tf.keras.models.load_model("./Models/model_lstm_attent
 #Vocabularies 
 with open("./vocabulary/vocabulary_language_detector", "rb") as file:
     vocabulary_language_detector = pickle.load(file)
+with open("./vocabulary/vocabulary_es", "rb") as file:
+    vocabulary_es = pickle.load(file)
 with open("./vocabulary/vocabulary_en", "rb") as file:
     vocabulary_en = pickle.load(file)
 
 #Stopwords
 with open("./vocabulary/stopwords_language_detector", "rb") as file:
     stopwords_language_detector = pickle.load(file)
+with open("./vocabulary/stopwords_es", "rb") as file:
+    stopwords_es = pickle.load(file)
 with open("./vocabulary/stopwords_en", "rb") as file:
     stopwords_en = pickle.load(file)
-
+    
+app.include_router(language_detector)
 app.include_router(language_detector_mlp)
 app.include_router(language_detector_lstm)
 app.include_router(language_detector_gru)
-app.include_router(language_detector)
+app.include_router(opinion_es_lstm)
 app.include_router(opinion_en_lstm)
 app.include_router(opinion_en_gru)
 app.include_router(opinion_en_cnn)
