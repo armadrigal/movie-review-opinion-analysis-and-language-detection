606a29a6d35f955d409bf0003474fcae5f73296a
armad-hermann <amadrigal.fismat@gmail.com>
2023-11-19 21:03:20 -0600

    se agregó DataModels

---
+++ API/api/DataModels.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from typing import List
+
+class InputData(BaseModel):
+    id: int
+    text: str
+
+class InputTexts(BaseModel):
+    id: int
+    text: str
+
+class InputDataCustomLanguage(BaseModel):
+    model: str
+    texts: List[InputTexts]
+
+class OutputDataOpinion(BaseModel):
+    id: int
+    opinion: float
+
+class OutputDataLanguage(BaseModel):
+    id: int
+    es: float
+    en: float
+    fr: float
+    pt: float
+    it: float
+
+class LanguagePrediction(BaseModel):
+    es: float
+    en: float
+    fr: float
+    pt: float
+    it: float
+
+class OutputDataAllModels(BaseModel):
+    id: int
+    mlp: LanguagePrediction
+    lstm: LanguagePrediction
+    gru: LanguagePrediction
