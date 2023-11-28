6e267f742061c0de7e77b739f8b648ad33464853
armad-hermann <amadrigal.fismat@gmail.com>
2023-11-27 23:12:59 -0600

    se agregó opinion_it_lstm

---
+++ API/api/opinion_it_lstm.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from typing import List
+from DataModels import InputData, OutputDataOpinion
+from process_data import ProcessData
+import main
+import numpy as np
+
+router = APIRouter()
+
+@router.post("/OpinionAnalysis/it/lstm")
+async def opinion_it_lstm(data: List[InputData]):
+
+    texts = [item.text for item in data]
+    texts = ProcessData(texts, main.vocabulary_it, main.stopwords_it, language='it')
+    y_pred = np.round(main.model_lstma_it(texts), 2)
+
+    responses = []
+    for i, item in enumerate(data):
+        output_data = OutputDataOpinion(id=item.id, opinion=round(y_pred[i,0],2))
+        responses.append(output_data)
+
+    return responses
