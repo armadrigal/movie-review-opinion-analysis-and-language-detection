990a50d793155eb5b59c18dc3cecef86971130ff
armad-hermann <amadrigal.fismat@gmail.com>
2023-11-27 23:24:16 -0600

    se agregó opinion_fr_attention

---
+++ API/api/opinion_fr_attention.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from typing import List
+from DataModels import InputData, OutputDataOpinion
+from process_data import ProcessData
+import main
+import numpy as np
+
+router = APIRouter()
+
+@router.post("/OpinionAnalysis/fr/attention")
+async def opinion_fr_attention(data: List[InputData]):
+
+    texts = [item.text for item in data]
+    texts = ProcessData(texts, main.vocabulary_fr, main.stopwords_fr, language='fr')
+    y_pred = np.round(main.model_attention_fr(texts), 2)
+
+    responses = []
+    for i, item in enumerate(data):
+        output_data = OutputDataOpinion(id=item.id, opinion=round(y_pred[i,0],2))
+        responses.append(output_data)
+
+    return responses
